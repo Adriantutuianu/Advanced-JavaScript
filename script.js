afea4ef76423a77e23c560a3bb35567b5788b665
Adrian <adriantutuianu01@gmail.com>
2021-05-21 22:42:44 +0100

    Concatanation

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ localFunction();
 // Global scope
 var globalVar = 5;
 function newFunction() {
+  globalVar = 6;
+
   console.log(globalVar);
 }
 console.log(globalVar);
@@ -18,19 +20,21 @@ newFunction();
 var newVar = 50;
 
 // String - sequence of characters
-var newStr = "Hey! I am a string.";
+var newString = "Hey! I am a string.";
 
 // Lenght of the string
 var newStr = "Hey String";
-var strLEnght = newStr.length;
-
+var strLenght = newStr.length;
+document.write(newStr);
 // Joining two strings -concat
 var string1 = "Hey";
 var string2 = "String";
 var string3 = string1.concat(string2);
-
+console.log("String lenght : " + strLenght);
 // Substring
 var myString = "I am in love with Strings.";
 // This will extract a string from the variable from position 5 upto length 7
 myString.substr(5, 7);
 // The above snippet will return "in love" - space is a character as well .
+
+// Converting string to uppercase and lowercase
